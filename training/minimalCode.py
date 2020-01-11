a65f4d9c5e9703192a1773c0df708071ce3fce9f
silhanr <silhanrom@gmail.com>
2020-01-11 21:00:38 +0100

    init - minimal code

--- training/minimalCode.py
+++ training/minimalCode.py
@@ -1,1 +1,30 @@
-pass
+# import pygame module
+import pygame
+
+
+class SimpleGame:
+    def __init__(self):
+        self.screen = None
+
+    def main(self):
+
+        self.open_window(860,640)
+
+        running = True
+
+        # main loop
+        while running:
+            # event handling, gets all event from the event queue
+            for event in pygame.event.get():
+                # only do something if the event is of type QUIT
+                if event.type == pygame.QUIT:
+                    # change the value to False, to exit the main loop
+                    running = False
+
+    def open_window(self, height, width):
+        self.screen = pygame.display.set_mode((height, width))
+
+
+if __name__ == "__main__":
+    game = SimpleGame()
+    game.main()
