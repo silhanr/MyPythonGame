b4648aafadeb25fde406fd73b6fa1552dcf2ad0d
silhanr <silhanrom@gmail.com>
2020-01-12 12:25:12 +0100

    arrows and space recognized and character is moving according to them

--- training/character.py
+++ training/character.py
@@ -1,18 +1,20 @@
 import pygame, os, sys, configparser
 
+STEP = 10
 
 class MainCharacter:
-    def __init__(self):
-
+    def __init__(self, position_x, position_y):
         self.root_path = "../../MyPythonGame"
         self.character_image = \
             pygame.image.load(os.path.join(self.root_path, "images", "big_smile.png"))
 
-        self.image_size = [50,50]
+        self.image_size = [50, 50]
+        self.resolution = [1000, 500]
+
+        self.position_x = position_x
+        self.position_y = position_y
 
 
-        self.position_x = 0
-        self.position_y = 0
 
     """
     def get_config(self,section,parameter):
@@ -20,3 +22,29 @@ class MainCharacter:
         config.read(os.path.join("self.root_path", "training", "config.ini"))
         return config.read("environment","height")
     """
+
+    def is_in_screen(self, position_x, position_y):
+        return (self.resolution[0] - self.image_size[0] >= position_x >= 0) \
+               and (self.resolution[1] - self.image_size[1] >= position_y >= 0)
+
+    def move_right(self):
+        if self.is_in_screen(self.position_x + STEP, self.position_y):
+            self.position_x = self.position_x + STEP
+        else:
+            print("Border!")
+
+    def move_left(self):
+        if self.is_in_screen(self.position_x - STEP, self.position_y):
+            self.position_x = self.position_x - STEP
+        else:
+            print("Border!")
+
+    def hit_space(self):
+        print("Space recorded!")
+        if self.is_in_screen(self.position_x, self.position_y - STEP)\
+                and self.resolution[1] <= self.position_y + self.image_size[1]:
+            self.position_y = self.position_y - STEP*2
+        else:
+            self.position_y = self.resolution[1] - self.image_size[1]
+
+
--- training/game.py
+++ training/game.py
@@ -5,17 +5,18 @@ import pygame, os, configparser
 
 from training.character import MainCharacter
 
+FPS = 60
+
 
 class SimpleGame:
     def __init__(self):
         self.root_path = "../../MyPythonGame"
 
-        self.main_screen = None
+        self.game_screen = None
         self.resolution = [1000, 500]
-        self.main_character = MainCharacter()
-
-
-
+        # self.game_character = MainCharacter(475, 450)
+        self.game_character = MainCharacter(475, 450)
+        self.clock = pygame.time.Clock()
 
         # initialize the pygame module
         pygame.init()
@@ -33,10 +34,44 @@ class SimpleGame:
             # event handling, gets all event from the event queue
             for event in pygame.event.get():
                 # only do something if the event is of type QUIT
-                if event.type == pygame.QUIT:
+                if event.type == pygame.QUIT \
+                        or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
                     # change the value to False, to exit the main loop
                     running = False
-            if self.main_character.position_x > \
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        print("move ->")
+                        self.game_character.move_right()
+                    if event.key == pygame.K_LEFT:
+                        print("move <-")
+                        self.game_character.move_left()
+                    if event.key == pygame.K_SPACE:
+                        print("space")
+                        self.game_character.hit_space()
+                        self.reload_background()
+
+                        self.game_screen.blit(self.game_character.character_image,
+                                              (self.game_character.position_x, \
+                                               self.game_character.position_y))
+
+                        # self.clock.tick(FPS)
+
+                        pygame.display.update()
+
+                        for i in range(5):
+                            self.clock.tick(FPS)
+
+                        self.game_character.hit_space()
+
+                    self.reload_background()
+                    self.game_screen.blit(self.game_character.character_image,
+                                          (self.game_character.position_x, \
+                                           self.game_character.position_y))
+
+            self.clock.tick(FPS)
+            pygame.display.update()
+
+            """if self.main_character.position_x > \
                 self.resolution[0]-self.main_character.image_size[0] \
                     or self.main_character.position_x < 0:
                 step_x = -step_x
@@ -52,32 +87,24 @@ class SimpleGame:
                                    self.main_character.position_y))
             #time.sleep(1)
             pygame.display.flip()
+            """
 
-
-
-
-
-
-
-    def open_window(self,width, height):
+    def open_window(self, width, height):
         """
         This method creates a gaming window
         :param height:
         :param width:
         :return:
         """
-        self.main_screen = pygame.display.set_mode((width,height))
+        self.game_screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Shoot the ball!")
 
-        #background
-        self.main_screen.blit(
-            pygame.image.load(
-                os.path.join(self.root_path, "images", "background.jpg")), (0,0))
+        # display background
+        self.reload_background()
 
-        pos_x = self.main_character.position_x
-        pos_y = self.main_character.position_y
-
-        self.main_screen.blit(self.main_character.character_image,
-                              (pos_x, pos_y))
+        self.game_screen.blit(self.game_character.character_image,
+                              (self.game_character.position_x,
+                               self.game_character.position_y))
 
         pygame.display.flip()
 
@@ -91,10 +118,13 @@ class SimpleGame:
         return [int(width),int(height)]
     |"""
 
+    def reload_background(self):
+        self.game_screen.blit(
+            pygame.image.load(
+                os.path.join(self.root_path, "images", "background.jpg")), (0, 0))
 
 
 if __name__ == "__main__":
     game = SimpleGame()
     # test 2
     game.main()
-
